b5ab238b544e8bb2d77cb0f3f58cf12ccd835f45
ankitxnarwal <ankitnarwal224@gmail.com>
2024-06-30 10:34:02 +0530

    adding current location function

--- weather-app/script.js
+++ weather-app/script.js
@@ -6,6 +6,7 @@ const cloudData = document.querySelector("#cloud-data");
 const cityData = document.querySelector("#city");
 const searchBtn = document.querySelector("#search-btn");
 const API_KEY = `3edd9b17ea78a017b027e5e023fa227f`;
+const limit = 1;
 function listen() {
   let city = document.querySelector("#input-city").value;
   fatchData(city);
@@ -28,3 +29,34 @@ async function fatchData(city) {
     console.error(error);
   }
 }
+// fatchData("Delhi");
+
+async function currentLocation() {
+  try {
+    if ("geolocation" in navigator) {
+      const position = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+
+      // const API_KEY = 'your_api_key_here'; // Replace with your actual API key
+      // const limit = 1; // Adjust as needed
+      const currLoc = await fetch(
+        `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=${limit}&appid=${API_KEY}`
+      );
+
+      const data = await currLoc.json();
+      console.log(data);
+      fatchData(data[0].name);
+    } else {
+      console.log("Geolocation is not available in this browser.");
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+currentLocation();
