5e5beda88775ffb9794cdfc69a6f80b19cf37af1
Ankit <147622085+anktxnarwal@users.noreply.github.com>
2024-07-06 17:43:52 +0530

    recipe app is completed

--- recipe-app/script.js
+++ recipe-app/script.js
@@ -1,6 +1,8 @@
 const searchBox = document.querySelector("#search-Box");
 const searchBtn = document.querySelector(".search-Btn");
 const recipeCon = document.querySelector(".recipe-container");
+const recipeDetailes = document.querySelector(".recipe-detail-container");
+const recipeCloseBtn = document.querySelector(".recipe-close-btn");
 
 const recipes = async (items) => {
   recipeCon.innerHTML = "Fetching Recipes";
@@ -22,10 +24,43 @@ const recipes = async (items) => {
     const button = document.createElement("button");
     button.textContent = "View Recipe";
     recipeDiv.appendChild(button);
+    button.addEventListener("click", () => {
+      openRecipePopup(meal);
+    });
     recipeCon.appendChild(recipeDiv);
   });
 };
+const fetchIngredients = (meal) => {
+  let ingredientList = "";
+  for (let i = 1; i <= 20; i++) {
+    const ingredient = meal[`strIngredient${i}`];
+    if (ingredient) {
+      const measure = meal[`strMeasure${i}`];
+      ingredientList += `<li>${measure} ${ingredient}</li>`;
+    } else {
+      break;
+    }
+  }
+  return ingredientList;
+};
+const openRecipePopup = (meal) => {
+  recipeDetailes.innerHTML = `
+  <h2 class="recipe-name">${meal.strMeal}</h2>
+  <h3>Ingredients:</h3>
+  <ul class="recipe-ingredientList">
+  ${fetchIngredients(meal)}
+  </ul>
+  <div class="recipe-instructions">
+    <h3>Instructions:</h3>
+    <p >${meal.strInstructions}</p>
+  </div>
+  `;
 
+  recipeDetailes.parentElement.style.display = "block";
+};
+recipeCloseBtn.addEventListener("click", () => {
+  recipeDetailes.parentElement.style.display = "none";
+});
 searchBtn.addEventListener("click", (e) => {
   e.preventDefault();
   const searchedItem = searchBox.value.trim();
