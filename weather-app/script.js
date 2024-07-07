4c99ec74c650bc35321f53b2da57f0d6ccc8df8b
Ankit <147622085+anktxnarwal@users.noreply.github.com>
2024-07-07 19:50:21 +0530

    update files

--- weather-app/script.js
+++ weather-app/script.js
@@ -18,7 +18,7 @@ async function fatchData(city) {
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
     );
     const result = await response.json();
-    console.log(result);
+    // console.log(result);
     cityData.innerText = result.name;
     mainTemp.innerText = result.main.temp;
     realFeel.innerText = result.main.feels_like;
@@ -40,16 +40,15 @@ async function currentLocation() {
 
       const latitude = position.coords.latitude;
       const longitude = position.coords.longitude;
-      console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+      // console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
 
-      const API_KEY = '3edd9b17ea78a017b027e5e023fa227f'; // Replace with your actual API key
-      const limit = 1; // Adjust as needed
+      const API_KEY = "3edd9b17ea78a017b027e5e023fa227f";
+      const limit = 2;
       const currLoc = await fetch(
         `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=${limit}&appid=${API_KEY}`
       );
 
       const data = await currLoc.json();
-      console.log(data);
       fatchData(data[0].name);
     } else {
       console.log("Geolocation is not available in this browser.");
